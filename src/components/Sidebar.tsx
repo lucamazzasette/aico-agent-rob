4b819b395842fe828f3cfdfe8c334e9efd1abe24
lucamazzasette <luca.mazzasette@comau.com>
2025-01-23 11:09:15 +0100

    Deploy UI

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -57,16 +57,16 @@ export default function Sidebar() {
       </div>
       
       <div className="flex flex-col gap-2">
-        {[
-          { icon: HelpCircle, label: "Help" },
+        {/*[
+          {icon: HelpCircle, label: "Help" },
           { icon: ActivityIcon, label: "Activity" },
-          { icon: SettingsIcon, label: "Settings" },
+          { icon: SettingsIcon, label: "Settings"},
         ].map((item, index) => (
           <div key={index} className={`flex items-start gap-2 cursor-pointer px-4 py-2 rounded-full text-gray-800 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors ${extended ? 'w-full' : 'w-auto'}`}>
             <item.icon size={20} />
             {extended && <p>{item.label}</p>}
           </div>
-        ))}
+        ))*/}
 
         <div className={`flex items-start gap-2 cursor-pointer px-4 py-2 rounded-full text-gray-800 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors ${extended ? 'w-full' : 'w-auto'}`}>
           <ThemeToggle />
